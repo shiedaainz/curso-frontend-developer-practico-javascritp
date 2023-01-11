ac6dba4aa41c01c404c5b4c6bbce94f514510746
shieda <uajfdhrsjaj34@gmail.com>
2023-01-11 17:48:22 -0500

    fusion del menu de compras

--- main.js
+++ main.js
@@ -1,7 +1,39 @@
 const menuemail= document.querySelector('.navbar-email')
 const desktomenu=document.querySelector('.desktop-menu')
+const menuhamicon=document.querySelector('.menu')
+const menucarritoicon=document.querySelector('.navbar-shopping-cart')
+const mobilemenu=document.querySelector('.mobile-menu')
+const aside=document.querySelector('.product-detail')
 menuemail.addEventListener('click',toggledesktopmenu)
+menuhamicon.addEventListener('click',togglemobilemenu)
+menucarritoicon.addEventListener('click',togglecarritomenu)
 
 function toggledesktopmenu(){
-  desktomenu.classList.toggle('inactive')  
+    const asideclose= aside.classList.contains('inactive')
+    if(!asideclose){
+        aside.classList.add('inactive')
+    }
+
+    desktomenu.classList.toggle('inactive')  
+}
+
+function togglemobilemenu(){
+    const asideclose= aside.classList.contains('inactive')
+    if(!asideclose){
+        aside.classList.add('inactive')
+    }
+    mobilemenu.classList.toggle('inactive')
+}
+
+function togglecarritomenu(){
+    const mobilemenuclose= mobilemenu.classList.contains('inactive')
+    const desktopmenuclose= desktomenu.classList.contains('inactive')
+    if(!mobilemenuclose){
+        mobilemenu.classList.add('inactive')
+    }
+    if(!desktopmenuclose){
+        desktomenu.classList.add('inactive')
+
+    }
+    aside.classList.toggle('inactive')
 }
